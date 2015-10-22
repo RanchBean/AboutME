8a68b2edacfd45b1384c24b118ca811875b09782
Ethan throndsen <Ethunderson1@gmail.com>
2015-10-22 13:46:21 -0600

    made it Work

--- app/src/main/java/ctec/aboutme/AutoActivity.java
+++ app/src/main/java/ctec/aboutme/AutoActivity.java
@@ -1,62 +1,66 @@
 package ctec.aboutme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.content.Intent;
 
 public class AutoActivity extends AppCompatActivity
 {
-    private ImageButton ToMusic;
-    private TextView Auto;
+	private ImageButton ToMusic;
+	private TextView Auto;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auto);
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_auto);
 
-        ToMusic = (ImageButton) findViewById(R.id.Equalizer);
-        Auto = (TextView) findViewById(R.id.Auto);
+		ToMusic = (ImageButton) findViewById(R.id.Equalizer);
+		Auto = (TextView) findViewById(R.id.Auto);
 
 
-        setupListeners();
-    }
+		setupListeners();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_auto, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_auto, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+		//noinspection SimplifiableIfStatement
+		if (id == R.id)
+		{
+			return true;
+		}
 
-        return super.onOptionsItemSelected(item);
-    }
-    private void setupListeners()
-    {
-        ToMusic.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View buttonView)
-            {
-                Intent changeScreen = new Intent(buttonView.getContext(), MusicActivity.class);
-                startActivityForResult(changeScreen, 0);
-            }
-        });
-    }
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void setupListeners()
+	{
+		ToMusic.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View buttonView)
+			{
+				Intent changeScreen = new Intent(buttonView.getContext(), MusicActivity.class);
+				startActivityForResult(changeScreen, 0);
+			}
+		});
+	}
 }
--- app/src/main/java/ctec/aboutme/MusicActivity.java
+++ app/src/main/java/ctec/aboutme/MusicActivity.java
@@ -1,49 +1,50 @@
 package ctec.aboutme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
-import android.content.Intent;
 
 public class MusicActivity extends AppCompatActivity
 {
 
-    private TextView Music;
+	private TextView Music;
 
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_music);
+		Music = (TextView) findViewById(R.id.Music);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_music);
-       Music = (TextView) findViewById(R.id.Music);
 
+	}
 
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_music, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_music, menu);
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings)
+		{
+			return true;
+		}
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+		return super.onOptionsItemSelected(item);
+	}
 
 }
